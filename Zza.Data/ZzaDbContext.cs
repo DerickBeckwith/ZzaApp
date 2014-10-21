3c7425965a3b9f81e1423aa81adee82a42294315
Derick Beckwith <wbeckwith@excellerx.com>
2014-10-21 11:44:47 -0400

    Add Zza.Data project and EntityFramework

---
+++ Zza.Data/ZzaDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Zza.Data
+{
+    using System.Data.Entity;
+
+    using Zza.Entities;
+
+    public class ZzaDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+    }
+}
