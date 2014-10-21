2df6b4f47b171f26fc058a51408ee97d9117862e
Derick Beckwith <wbeckwith@excellerx.com>
2014-10-21 11:31:55 -0400

    Add the ZzaService implementation class

---
+++ Zza.Services/ZzaService.cs
@@ -0,0 +1,24 @@
+﻿namespace Zza.Services
+{
+    using System.Collections.Generic;
+
+    using Zza.Entities;
+
+    public class ZzaService : IZzaService
+    {
+        public List<Product> GetProducts()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SubmitOrder(Order order)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
