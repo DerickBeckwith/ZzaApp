df6158f32163aef565b0d76050b073419a34da82
Derick Beckwith <wbeckwith@excellerx.com>
2014-10-21 10:50:50 -0400

    Define the service contract methods

---
+++ Zza.Services/IZzaService.cs
@@ -0,0 +1,21 @@
+﻿namespace Zza.Services
+{
+    using System.Collections.Generic;
+    using System.ServiceModel;
+
+    /// <summary>
+    /// Definition of the Pizza Delivery Service.
+    /// </summary>
+    [ServiceContract]
+    public interface IZzaService
+    {
+        [OperationContract]
+        List<Product> GetProducts();
+
+        [OperationContract]
+        List<Customer> GetCustomers();
+
+        [OperationContract]
+        void SubmitOrder(Order order);
+    }
+}
